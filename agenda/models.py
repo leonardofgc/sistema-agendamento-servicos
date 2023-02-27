df378e3453a3e34cd0ab20697ff046144616a607
Leonardo Guimarães <leonardofgc@gmail.com>
2023-02-27 19:26:02 -0300

    Adicionado Criar Agendamento

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Agendamento(models.Model):
+
+    def __str__(self) -> str:
+        return f"{self.nome_cliente}"
+
+    data_horario = models.DateTimeField()
+    nome_cliente = models.CharField(max_length=200)
+    email_cliente = models.EmailField()
+    telefone_cliente = models.CharField(max_length=20)
--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from agenda.models import Agendamento
+from agenda.serializers import AgendamentoSerializer
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(http_method_names=["GET"])
+def agendamento_detalhes(request, id):
+    agendamento = get_object_or_404(Agendamento, id=id)
+    serializer = AgendamentoSerializer(agendamento)
+    return JsonResponse(serializer.data)
+
+
+@api_view(http_method_names=["GET", "POST"])
+def agendamento_lista(request):
+    if request.method == "GET":
+        qs = Agendamento.objects.all()
+        serizalizer = AgendamentoSerializer(qs, many=True)
+        return JsonResponse(serizalizer.data, safe=False)
+    if request.method == "POST":
+        data = request.data
+        serizalizer = AgendamentoSerializer(data=data)
+        if serizalizer.is_valid():
+            validated_data = serizalizer.validated_data
+            Agendamento.objects.create(
+                data_horario = validated_data["data_horario"],
+                nome_cliente = validated_data["nome_cliente"],
+                email_cliente = validated_data["email_cliente"],
+                telefone_cliente = validated_data["telefone_cliente"]
+            )
+            return JsonResponse(serizalizer.data, status=201)
+        return JsonResponse(serizalizer.errors, status=400)
+    
